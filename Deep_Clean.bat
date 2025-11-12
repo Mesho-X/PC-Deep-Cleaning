a2b826c1d451680a5903190ed57b4e597215ee1e
Mesho <Talal82hh@gmail.com>
2025-11-12 19:58:46 +0300

    Update Deep_Clean.bat

--- Deep_Clean.bat
+++ Deep_Clean.bat
@@ -12,50 +12,52 @@ echo Press (N) to ENABLE Windows Update
 echo Press (C) to CONTINUE without changing the service
 choice /C YNC /N /M "Select your option:"
 
-IF ERRORLEVEL 3 GOTO NO_CHANGE
-IF ERRORLEVEL 2 GOTO ENABLE_UPDATE
-IF ERRORLEVEL 1 GOTO DISABLE_UPDATE
+IF ERRORLEVEL 3 GOTO START_CLEANING  :: C (Continue)
+IF ERRORLEVEL 2 GOTO ENABLE_UPDATE    :: N (Enable)
+IF ERRORLEVEL 1 GOTO DISABLE_UPDATE   :: Y (Disable)
 
 :DISABLE_UPDATE
-    NET STOP wuauserv
     SC config wuauserv start= disabled
+    NET STOP wuauserv
     echo.
     echo [SUCCESS] Windows Update Service Disabled.
-    GOTO START_CLEANING
+    GOTO CLEANUP_SWD
 
 :ENABLE_UPDATE
     SC config wuauserv start= auto
     NET START wuauserv
     echo.
     echo [SUCCESS] Windows Update Service Enabled.
-    GOTO START_CLEANING
-
-:NO_CHANGE
-    echo.
-    echo [INFO] Continuing without changing Update Service state.
-    GOTO START_CLEANING
+    GOTO CLEANUP_SWD
     
 :: --- 2. Core Cleaning Process ---
 :START_CLEANING
-echo.
-echo Starting Deep System Cleanup...
-DEL /F /S /Q "%TEMP%\*.*"
-DEL /F /S /Q "C:\Windows\Temp\*.*"
-DEL /F /S /Q "C:\Windows\Prefetch\*.*"
-DEL /F /S /Q "C:\Windows\Logs\*.*"
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+    echo.
+    echo [INFO] Skipping Windows Update state change.
+    GOTO CLEANUP_SWD
 
-:: Software Distribution Purge
-IF ERRORLEVEL 2 GOTO SKIP_SWD
+:CLEANUP_SWD
+    echo.
+    echo Starting Deep System Cleanup...
+    
+    :: Cleaning Temp files
+    DEL /F /S /Q "%TEMP%\*.*"
+    DEL /F /S /Q "C:\Windows\Temp\*.*"
+    DEL /F /S /Q "C:\Windows\Prefetch\*.*"
+    DEL /F /S /Q "C:\Windows\Logs\*.*"
+    RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+
+    :: Renaming SoftwareDistribution (Removes old updates cache)
+    echo [INFO] Purging old Windows Update cache...
     NET STOP wuauserv
     REN "C:\Windows\SoftwareDistribution" "C:\Windows\SoftwareDistribution.old"
     NET START wuauserv
-:SKIP_SWD
-
-rd /s /q C:\$Recycle.bin
-md C:\$Recycle.bin
-cleanmgr /sageset:1
-cleanmgr /sagerun:1
-echo Cleanup complete. System rebooting in 10 seconds (Ctrl+C to abort).
-shutdown /r /t 10
-pause > nul
+    
+    rd /s /q C:\$Recycle.bin
+    md C:\$Recycle.bin
+    cleanmgr /sageset:1
+    cleanmgr /sagerun:1
+    
+    echo Cleanup complete. System rebooting in 10 seconds (Ctrl+C to abort).
+    shutdown /r /t 10
+    pause > nul
